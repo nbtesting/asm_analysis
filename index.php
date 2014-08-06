8ee4eeb38f6b47df47ed66381affe9375cfdea6d
briantduong <briantduong@gmail.com>
2014-08-06 08:59:49 -0500

    centering form
    added main.css, centered “search user” form

--- index.php
+++ index.php
@@ -9,11 +9,12 @@ echo <<<_END
 
 	<head>
 		<title>ASM Analysis</title>
+		<link rel="stylesheet" href="main.css">
 	</head>
 	
 	<body>
 		Your name is $name<br />
-		<form method="post" action="formtest.php">
+		<form method="post" action="analysis.php">
 			Search User:
 			<input type="text" name="name" size="30" maxlength="35" value="Firstname Lastname"/>
 			<input type="submit" />
