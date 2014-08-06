e7676d57956576bfe5dfe9a0de59d247cf451f09
nbtesting <Neil>
2014-08-06 10:10:50 -0500

    added connection info and POST method

--- analysis.php
+++ analysis.php
@@ -1,4 +1,12 @@
 <?php // analysis.php
-
-
+	$name = $_POST['name'];
+//posts the user input
+	$db=mysql_connect ('localhost','root','') or die('I cannot connect to the database because: ' . mysql_error());
+	$mydb=mysql_select_db('NBTest');
+//connection
+	$strSQL = "SELECT * FROM members where firstname = '$firstname'";
+	$rs = mysql_query($strSQL);
+		while($row = mysql_fetch_array($rs)){
+			echo $row['name'];
+		}
 ?>
